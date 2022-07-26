1719318d78f2eb782a532948c0a09a8811db1cfb
damatoaj <mr.arthurdamato@gmail.com>
2022-07-26 07:12:52 -0700

    Set up project

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp} from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyComvIpi1BoLB4i7_JAiLeYULYgbIkm5BE",
+    authDomain: "booklist-udemy.firebaseapp.com",
+    projectId: "booklist-udemy",
+    storageBucket: "booklist-udemy.appspot.com",
+    messagingSenderId: "790189193893",
+    appId: "1:790189193893:web:43ee29b9fa6f67f03d2b4c"
+  };
+
+//init firebase like so
+initializeApp(firebaseConfig);
+
+//init firestore
+const db = getFirestore();
+
+export { db }
+
